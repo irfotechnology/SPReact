930719c24526065064c0541f5d49fbf18bbbb999
Irfan Shaikh <irfotechnology@gmail.com>
2024-07-08 11:49:56 +0530

    changes made to update static path with SP CDN in the files

--- spreact-app-package/bin/spreact-app-package.js
+++ spreact-app-package/bin/spreact-app-package.js
@@ -1,37 +1,62 @@
 #!/usr/bin/env node
 
+function updateStaticCDNPath(dirTemp, cndPath){
+    const dirStatic = fs.realpathSync(`${dirTemp}/static`);
+    const dirSub = fs.readdirSync(dirStatic,{recursive: true});
+    dirSub.forEach(dir => {
+        const files = fs.readdirSync(`${dirStatic}/${dir}`,{recursive:true});
+        files.forEach(file => {
+            const file_content = fs.readFileSync(`${dirStatic}/${dir}/${file}`,{encoding:'utf8'});
+            const new_file_content = file_content.replace(/static/g, cndPath);
+            fs.writeFileSync(`${dirStatic}/${dir}/${file}`,new_file_content,{encoding:'utf8'});
+        });
+    });
+}
+
 console.log("Creating application package.... please wait...");
 const currentDir = process.cwd();
 
 console.log(`working directory : ${currentDir} `);
-
 const fs = require('node:fs');
-
 
 
 const dirDeploy = `${currentDir}/deploy`
 const dirTemp = `${dirDeploy}/temp`;
 const dirBuild = `${currentDir}/build`;
-
-const packManifest = JSON.parse(fs.readFileSync(`${currentDir}/config/app-package.json`));
-const appManifest = packManifest.app
-
 try {
+
+    const assetManifest = JSON.parse(fs.readFileSync(`${dirBuild}/asset-manifest.json`));
+    const mainJS = assetManifest.files['main.js'];
+    const mainCss = assetManifest.files['main.css'];
+    const packManifest = JSON.parse(fs.readFileSync(`${currentDir}/config/app-package.json`));
+    const appManifest = packManifest.app;
+    const sharePoint = packManifest.sharepointHost;
+    const staticCDNPath = `/publiccdn.sharepointonline.com/rotarex.${sharePoint}.com/ClientSideApps/${appManifest.id}/static/`
+
     if (!fs.existsSync(dirDeploy)) {
         fs.mkdirSync(dirDeploy);
     }
     else {
         fs.rmSync(dirDeploy, { recursive: true, force: true });
         fs.mkdirSync(dirDeploy);
+        fs.mkdirSync(`${dirDeploy}/temp`);
     }
-    const outDirPath = fs.realpathSync(dirDeploy);
+
     //Copy build files
     fs.cpSync(dirBuild, dirTemp, { recursive: true, force: true });
+    //console.log('Build files copied to temp.');
+    //update static/ path
+    updateStaticCDNPath(dirTemp,staticCDNPath);
+    //console.log('Updated static cdn path.');
+    //copy main.js file
+    fs.copyFileSync(fs.realpathSync(`${dirTemp}${mainJS}`),`${dirTemp}/main.js`);
+    //copy main.css file
+    fs.copyFileSync(fs.realpathSync(`${dirTemp}${mainCss}`),`${dirTemp}/main.css`);
     //Copy app manifest file
     fs.writeFileSync(`${dirTemp}/appManifest.json`, JSON.stringify(appManifest));
     //create app pacakge
     var archiver = require('archiver');
-
+    const outDirPath = fs.realpathSync(dirDeploy);
     var output = fs.createWriteStream(`${outDirPath}/${packManifest.appPackagename}`);
     var archive = archiver('zip');
 
@@ -42,6 +67,7 @@ try {
     });
 
     archive.on('error', function (err) {
+        console.error(err);
         throw err;
     });
 
